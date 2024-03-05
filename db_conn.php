801bd5179e38e0195ed0e7fe46b05826f7de599b
Sandeep pradhan <112794288+SandeepPradhan1@users.noreply.github.com>
2024-03-05 08:36:56 +0545

    .ht

--- db_conn.php
+++ db_conn.php
@@ -1,13 +1,1 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "web";
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-if(!$conn){
-    die("connection failed " . mysqli_connect_error());
-}
-// echo "Connected successfully";
-?>
